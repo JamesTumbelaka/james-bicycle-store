26968854ff25448afc0138541ade718d562530ed
JamesTumbelaka <jamesztumbelaka@gmail.com>
2023-09-18 14:10:03 +0700

    added remove product feature

--- main/models.py
+++ main/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
+    amount = models.IntegerField()
     date_added = models.DateField(auto_now_add=True)
     description = models.TextField()
