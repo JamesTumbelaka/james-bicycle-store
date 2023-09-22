39695bbbf0e77f939508ebd4eea95d4eefc0e8dc
JamesTumbelaka <jamesztumbelaka@gmail.com>
2023-09-22 15:56:08 +0700

    selesai parsial tugas 4

--- main/views.py
+++ main/views.py
@@ -5,7 +5,14 @@ from main.models import Product
 from django.urls import reverse
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
 
+@login_required(login_url='/login')
 def show_main(request):
     products = Product.objects.all()
     total_items = Product.objects.count()
@@ -14,6 +21,7 @@ def show_main(request):
         'class': 'PBP E',
         'products': products,
         'total_items': total_items,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -52,3 +60,36 @@ def delete_product(request, id):
         return redirect('main:show_main')
 
     return render(request, 'delete_product.html', {'product': product})
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
